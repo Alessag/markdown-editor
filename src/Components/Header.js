8e964e1e589e4205e0209706f233db093f9ddf3e
Alessandra <alessamica@gmail.com>
2020-05-31 01:59:38 -0400

    Basic styles header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
+const style = css`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+  background-color: pink;
+`;
 function Header() {
-  return <h1>Hola, soy el header</h1>;
+  return (
+    <header css={style}>
+      <div>Markdown Editor</div>
+      <div>Icon</div>
+    </header>
+  );
 }
 
 export default Header;
