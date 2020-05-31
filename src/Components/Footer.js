b74c10b9a7f36ed6ca07c5a12bb135e7cf3d38a2
Alessandra <alessamica@gmail.com>
2020-05-31 01:58:31 -0400

    Some footer styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+
+const style = css`
+  background-color: red;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+`;
 function Footer() {
   return (
-    <footer>
-      <h1>Hola, soy el footer</h1>
+    <footer css={style}>
+      <h1>Hey I am the footer!</h1>
     </footer>
   );
 }
