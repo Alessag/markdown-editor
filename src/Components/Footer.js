1bbe758b0a0cb435e60c1d09951a09f2027dcd75
Alessandra <alessamica@gmail.com>
2020-06-03 17:34:41 -0400

    Footer styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,46 +3,42 @@ import React from 'react';
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 
-const style = css`
-  background-color: red;
-  padding: 20px 0;
-  position: absoulte;
-  text-align: center;
-  bottom: 0px;
-  overflow: hidden;
-  widith: 100%;
-  color: white;
-`;
-
-const aStyles = css`
-  text-decoration: none;
-  color: white;
-`;
-
 function Footer() {
   return (
-    <footer css={style}>
-      <span>{`</>`} </span>
-      <span>
-        with{' '}
-        <a
-          css={aStyles}
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          React.js{' '}
-        </a>
-        by{' '}
-        <a
-          css={aStyles}
-          href='hhttps://github.com/Alessag'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          Alessandra
-        </a>
-      </span>
+    <footer>
+      <div
+        className='footer-description'
+        css={css`
+          padding: 16px 0px;
+          overflow: hidden;
+          position: absolute;
+          width: 100%;
+          text-align: center;
+          bottom: 0px;
+          color: #eee;
+          background: #000;
+        `}
+      >
+        <span>{`</>`} </span>
+        <span>
+          with{' '}
+          <a
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            React.js{' '}
+          </a>
+          by{' '}
+          <a
+            href='https://github.com/Alessag'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            Alessandra
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
