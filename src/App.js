7b9e19de9a4c28b5be650cc68a731b66752577fe
Alessandra <alessamica@gmail.com>
2020-06-03 17:48:17 -0400

    Switch theme and imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Main from './Components/Main';
+import useDarkMode from './useDarkMode';
 
 function App() {
+  const { theme, toggleTheme } = useDarkMode();
+  const themeStyle =
+    theme === 'light'
+      ? {
+          backgroundColor: '#eee',
+          color: '#000',
+        }
+      : {
+          backgroundColor: '#171616',
+          color: '#fff',
+        };
   return (
-    <div>
-      <Header />
-      <h1>Hola, soy Alessandra</h1>
+    <div className='App' style={themeStyle}>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <Main theme={theme} />
       <Footer />
     </div>
   );
