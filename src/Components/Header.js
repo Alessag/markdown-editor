08dfabb0630b753a37821a1040ce10fed9201bf0
Alessandra <alessamica@gmail.com>
2020-06-03 17:58:12 -0400

    Header styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,54 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
+import { FiSun } from 'react-icons/fi';
+import { FaMoon } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
-const style = css`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 0;
-  background-color: pink;
-`;
-function Header() {
+function Header({ theme, toggleTheme }) {
   return (
-    <header css={style}>
+    <header
+      css={
+        theme === 'dark'
+          ? css`
+              display: flex;
+              flex-direction: row;
+              justify-content: space-between;
+              background-color: #553abf;
+              padding: 24px 32px;
+              font-size: 16px;
+            `
+          : css`
+              display: flex;
+              flex-direction: row;
+              justify-content: space-between;
+              background-color: #41f8f8;
+              padding: 24px 32px;
+              box-shadow: 0px -2px 8px #000;
+              font-size: 16px;
+            `
+      }
+    >
       <div>Markdown Editor</div>
-      <div>Icon</div>
+      <div
+        role='presentation'
+        css={css`
+          cursor: pointer;
+        `}
+        onClick={toggleTheme}
+      >
+        {theme === 'dark' ? <FaMoon /> : <FiSun />}
+      </div>
     </header>
   );
 }
+
+Header.propTypes = {
+  theme: PropTypes.string.isRequired,
+  toggleTheme: PropTypes.func.isRequired,
+};
 
 export default Header;
